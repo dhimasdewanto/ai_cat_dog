803b01036e66557080e9c78ddb5248a0a2c54772
Dhimas Dewanto <dhimasbrd@gmail.com>
2020-09-22 00:26:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AI Cat or Dog App
+
+Learn AI by create cat or dog app detection.
+
+## Getting Started
+
+- [Udemy](https://www.udemy.com/course/flutter-deeplearning-course/learn/lecture/21918894#overview)
---
+++ lib/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Detect Cats and Dogs"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FaIcon(FontAwesomeIcons.cat, size: 100),
+                SizedBox(width: 10),
+                FaIcon(FontAwesomeIcons.dog, size: 100),
+              ],
+            ),
+            SizedBox(height: 30),
+            RaisedButton(
+              onPressed: () {},
+              child: const Text("Take a Photo"),
+            ),
+            SizedBox(height: 10),
+            RaisedButton(
+              onPressed: () {},
+              child: const Text("Camera Roll"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
